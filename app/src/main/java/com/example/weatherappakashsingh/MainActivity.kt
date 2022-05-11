93a16c8e971005c7fdac2aa6963e573a5dd4c069
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-05-11 17:58:52 +0530

    final app's init

--- app/src/main/java/com/example/weatherappakashsingh/MainActivity.kt
+++ app/src/main/java/com/example/weatherappakashsingh/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapp
+package com.example.weatherappakashsingh
 
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
--- app/src/test/java/com/example/weatherappakashsingh/ExampleUnitTest.kt
+++ app/src/test/java/com/example/weatherappakashsingh/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapp
+package com.example.weatherappakashsingh
 
 import org.junit.Test
 
